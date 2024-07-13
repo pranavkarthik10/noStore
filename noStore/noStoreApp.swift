73db0c276d285ab738740ed0c364f1b6c41052c0
pmruthyu <pranav.karthik10@gmail.com>
2024-07-13 18:54:20 -0400

    Initial Commit

---
+++ noStore/noStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  noStoreApp.swift
+//  noStore
+//
+//  Created by Pranav Karthik on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct noStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
